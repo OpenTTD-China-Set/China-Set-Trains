5c02afa2fa06e0ee10918f1720c4afbc7adf1fac
BabeL <tongtianta4444@gmail.com>
2024-08-14 19:37:52 +0800

    Expand compatibility

--- str.py
+++ str.py
@@ -19,14 +19,17 @@ with open("lang/english.lng", 'w', encoding='utf-8') as f:
         else:
             f.write(f"{strs[i][0]:48} {":"+strs[i][1]:<60}\n")
 
-with open("lang/simplified_chinese.lng", 'w', encoding='utf-8') as f:
-    f.write("##grflangid 0x56"+"\n")
-    for i in range(len(strs)):
-        if strs[i][2] == None:
-            if strs[i][1] == None:
-                f.write(strs[i][0]+"\n")
+def other_lang_generate(lang_name, lang_row, lang_code):
+    with open("lang/"+lang_name+".lng", 'w', encoding='utf-8') as f:
+        f.write("##grflangid "+lang_code+"\n")
+        for i in range(len(strs)):
+            if strs[i][lang_row] == None:
+                if strs[i][1] == None:
+                    f.write(strs[i][0]+"\n")
+                else:
+                    pass
             else:
-                pass
-        else:
-            f.write(f"{strs[i][0]:48} {":"+strs[i][2]:<60}\n")
+                f.write(f"{strs[i][0]:48} {":"+strs[i][2]:<60}\n")
+   
+other_lang_generate("simplified_chinese", 2, "0x56")
    
